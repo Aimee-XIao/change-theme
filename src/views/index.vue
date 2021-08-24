d0442b354466a0ffee4a596edab69e06d7a93239
wangyuxiao <wyx03140827ym@163.com>
2021-08-24 17:19:59 +0800

    主题切换

---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      menuList: [
+        {
+          url: '',
+          name: ''
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
